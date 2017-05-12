161f9603030d2ae207e687e8832611b8b7090dbe
Angular CLI <angular-cli@angular.io>
2017-05-13 00:33:32 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CvProjectPage } from './app.po';
+
+describe('cv-project App', () => {
+  let page: CvProjectPage;
+
+  beforeEach(() => {
+    page = new CvProjectPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('rb works!');
+  });
+});
